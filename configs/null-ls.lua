4e8160df04713fd0643b527ecd8d2a58f041a988
lavi-wsl <landa.lavi@gmail.com>
2023-10-25 05:36:03 +0300

    Fixed indenting

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -8,14 +8,14 @@ local opts = {
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.ruff,
         -- c
-        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.clang_format.with({ extra_args = { "--style={IndentWidth: 4}" } }),
         -- go
         null_ls.builtins.formatting.gofumpt,
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.golines,
         -- js + ts
         null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({ disabled_filetypes = { "markdown" }, extra_args = { "--tab-width", "4" } }),
     },
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
